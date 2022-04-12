350778d29f02383a654c8ee138ad2e887b2aa02a
galileor97 <galwicakso@gmail.com>
2022-04-13 03:28:11 +0700

    Mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import Layout from '../components/Layout.js'
 import '../styles/globals.css'
+import Head from 'next/head';
+
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
+    <Head>
+        <title>DTTH Labs</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     <Component {...pageProps} />
   </Layout>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         ragas: 'Ragas, cursive',
         mono: 'Mono, mono',
         reno: ['Reno Mono', 'mono']
+      },
+      boxShadow: {
+        'button' : '0px 4px 4px rgba(255, 3, 3, 0.25)'
       }
     },
   },

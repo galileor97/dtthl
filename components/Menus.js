c249c9a3bd0eaf8560235edd38c9f41bb4201d01
galileor97 <galwicakso@gmail.com>
2022-04-13 01:31:30 +0700

    hover color

--- components/Menus.js
+++ components/Menus.js
@@ -6,16 +6,16 @@ function Menus() {
     <div className="fixed w-full bottom-0 lg:px-5 px-5 lg:pb-20 z-70">
     <div className="hidden lg:flex h-full border-b border-white items-center justify-center max-w-11xl mx-auto border-opacity-0 space-x-10">
       <Link href="/">
-        <a className="text-5xl text-white font-libre hover:text-[#D45B27]">Home</a>
+        <a className="text-5xl text-white font-libre hover:text-[#EA3131]">Home</a>
       </Link>
       <Link href="/team">
-        <a className="text-5xl text-white font-libre  hover:text-[#D45B27]"> Team</a>
+        <a className="text-5xl text-white font-libre  hover:text-[#EA3131]"> Team</a>
       </Link>
       <Link href="/careers">
-        <a className="text-5xl text-white font-libre hover:text-[#D45B27]"> Careers</a>
+        <a className="text-5xl text-white font-libre hover:text-[#EA3131]"> Careers</a>
       </Link>
       <Link href="/about">
-        <a className="text-5xl text-white font-libre hover:text-[#D45B27]"> About</a>
+        <a className="text-5xl text-white font-libre hover:text-[#EA3131]"> About</a>
       </Link>
     </div>
   </div>
